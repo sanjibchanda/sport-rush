2cca33b0998a8d3d96eac117046be0b33d97e95c
sanjibchanda <sanjibchanda07@gmail.com>
2025-07-20 00:47:23 +0530

    slick slider css changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // import "./lib/slick.js";
 // Optionally import CSS
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import $ from "jquery";
 import "slick-carousel";
